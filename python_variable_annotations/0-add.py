4a176a88190746110f0289b0a2076a860523ddae
anthhoang <anthhoan@gmail.com>
2025-07-30 16:18:02 +1000

    Basic annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """0. Basic annotations - add"""
 
+
 def add(a: float, b: float) -> float:
     """"type-annotated function add
     that takes a float a and a float b as arguments
