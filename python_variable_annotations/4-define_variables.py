2e116c602e41ef6144d40a3c4835950ef04c6c4a
anthhoang <anthhoan@gmail.com>
2025-08-05 10:47:21 +1000

    Define variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -3,6 +3,10 @@
 
 
 a: int = 1
+"""a, an integer with a value of 1"""
 pt: float = 3.14
+"""pt, a float with a value of 3.14"""
 i_understand_annotations: bool = True
+"""i_understand_annotations, a boolean with a value of True"""
 school: str = "Holberton"
+"""school, a string with a value of "Holberton"/"""
