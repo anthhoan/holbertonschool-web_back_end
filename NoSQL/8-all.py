0c48e618328580e01bc3dfc91f1746544f8e29cd
anthhoang <anthhoan@gmail.com>
2025-08-14 14:49:57 +1000

    List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Function that lists all documents in a collection"""
+"""8. List all documents in Python"""
 import pymongo
 
 
 def list_all(mongo_collection):
+    """Function that lists all documents in a collection"""
     return list(mongo_collection.find())
