6d56240a9d1e437aa24d7a1178d09d96d89d903e
anthhoang <anthhoan@gmail.com>
2025-08-14 15:09:26 +1000

    Insert a document Python

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -6,4 +6,3 @@ import pymongo
 def insert_school(mongo_collection, **kwargs):
     """Function that inserts a new document in a collection based on 'kwargs'"""
     return (mongo_collection.insert_one(kwargs))
-    
