1ebffb5cbd2c8ff336ed18163b06716b5b90df37
anthhoang <anthhoan@gmail.com>
2025-09-05 12:41:10 +1000

    For...of Loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const arr = []
-		let idx = 0
+    const arr = [];
+		let idx = 0;
   for (const value of array) {
     arr[idx] = appendString + value;
 		idx += 1;
